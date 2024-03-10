f5f68e1a47d0c25abbaec6d7dce4a795be1ae148
CodingGoku <88716307+CodingGoku@users.noreply.github.com>
2024-03-10 20:01:15 +0530

    Add files via upload

---
+++ pokerHandDetector.py
@@ -0,0 +1,69 @@
+import cv2
+import cvzone
+from ultralytics import YOLO
+import math
+import pokerHandFunction
+
+cap = cv2.VideoCapture(0); # 0 for primary camera
+cap.set(3, 1280); # width 640/1280
+cap.set(4, 640); # height 480/640
+
+#cap = cv2.VideoCapture('Videos/cards.mp4') # for video result at (450,45)
+
+model = YOLO("../yoloWeights/playingCards.pt")
+
+
+classNames = ['10C', '10D', '10H', '10S', 
+              '2C', '2D', '2H', '2S', 
+              '3C', '3D', '3H', '3S', 
+              '4C', '4D', '4H', '4S', 
+              '5C', '5D', '5H', '5S', 
+              '6C', '6D', '6H', '6S', 
+              '7C', '7D', '7H', '7S', 
+              '8C', '8D', '8H', '8S', 
+              '9C', '9D', '9H', '9S', 
+              'AC', 'AD', 'AH', 'AS', 
+              'JC', 'JD', 'JH', 'JS', 
+              'KC', 'KD', 'KH', 'KS', 
+              'QC', 'QD', 'QH', 'QS']
+
+
+while True:
+    success, img = cap.read()
+    if not success:
+        print("Failed to read frame from video source")
+        break
+
+    results = model(img, stream=True)
+    hand = []
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1,y1,x2,y2 = box.xyxy[0]
+            x1,y1,x2,y2 = int(x1), int(y1), int(x2), int(y2)
+            #cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3) # corrdinates, colour, thickness
+            #x1,y1,w,h = box.xyxy[0]
+            #bbox = int(x1), int(y1), int(w), int(h)
+            #print(x1,y1,x2,y2)
+            w, h = x2-x1, y2-y1
+            cvzone.cornerRect(img,(x1,y1,w,h), l=9) #bounding box corner length=9
+
+            conf = math.ceil((box.conf[0]*100))/100 #confidence level
+            
+            #class name
+            cls = int(box.cls[0])
+            cvzone.putTextRect(img, f'{classNames[cls]} {conf}',(max(0,x1),max(35,y1)), scale = 1, thickness = 1) # position of confidence
+
+            # threshold
+            if conf>0.5:
+                hand.append(classNames[cls])
+
+    print(hand)
+    hand = list(set(hand))
+
+    if len(hand) ==5:
+        results = pokerHandFunction.findPokerHand(hand)
+        print(results)
+        cvzone.putTextRect(img, f'Your Hand: {results}',(300,55), scale = 3, thickness = 5)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
